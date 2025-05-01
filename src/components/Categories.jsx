1a1bb9938e03ad90a086743382d20101ab8447a9
Miguel Cavada <70104762+mangoPixel1@users.noreply.github.com>
2025-05-01 11:58:08 -0700

    Update Categories.jsx
    Changed aspect ratio to square and increased padding for small screens

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,63 +2,109 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Categories() {
-	const [categories, setCategories] = useState([]); // objects with the properties: name, image
+  const [categories, setCategories] = useState([]); // objects with the properties: name, image
 
-	useEffect(() => {
-		fetch(`https://fakestoreapi.com/products`)
-			.then(response => {
-				return response.json();
-			})
-			.then(data => {
-				// Extract all categories from products list, avoid duplicate names
-				let categoryNames = [];
-				data.forEach(item => {
-					if (!categoryNames.includes(item.category)) {
-						categoryNames.push(item.category);
-					}
-				});
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // Extract all categories from products list, avoid duplicate names
+        let categoryNames = [];
+        data.forEach((item) => {
+          if (!categoryNames.includes(item.category)) {
+            categoryNames.push(item.category);
+          }
+        });
 
-				// Capitalize first letter of each category
-				const names = categoryNames.map(name => {
-					return name.charAt(0).toUpperCase() + name.slice(1);
-				});
+        // Capitalize first letter of each category
+        const names = categoryNames.map((name) => {
+          return name.charAt(0).toUpperCase() + name.slice(1);
+        });
 
-				//console.log(names);
+        //console.log(names);
 
-				// Loop through products and find first matching product for each category
-				let categoryData = [];
-				categoryNames.forEach(name => {
-					const product = data.find(product => product.category === name);
-					const newCategoryEntry = {
-						name: name.charAt(0).toUpperCase() + name.slice(1),
-						image: product.image
-					};
-					categoryData.push(newCategoryEntry);
-				});
+        // Loop through products and find first matching product for each category
+        let categoryData = [];
+        categoryNames.forEach((name) => {
+          const product = data.find((product) => product.category === name);
+          const newCategoryEntry = {
+            name: name.charAt(0).toUpperCase() + name.slice(1),
+            image: product.image,
+          };
+          categoryData.push(newCategoryEntry);
+        });
 
-				//console.log(categoryData);
+        //console.log(categoryData);
 
-				setCategories(categoryData);
-			})
-			.catch(error => console.error(error));
-	}, []);
+        setCategories(categoryData);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
-	return (
-		<section className="px-8 py-15 lg:px-10">
-			<h1 className="text-slate-800 mb-15 lg:mb-10 font-semibold text-3xl lg:text-4xl text-center">Shop by Category</h1>
-			<div className="grid grid-cols-1 gap-14 md:grid-cols-4 md:gap-10 lg:max-w-3/4 lg:mx-auto">
-				{categories &&
-					categories.map((cat, index) => (
-						<Link to={`category/${cat.name.toLowerCase()}`} key={index}>
-							<div className="flex flex-col items-center gap-5 px-5 py-0 lg:py-12" key={index}>
-								<img className="w-full max-w-72 max-h-72 md:max-w-48 md:max-h-48 object-contain" src={cat.image} />
-								<span className="text-slate-700 text-lg font-medium">{cat.name}</span>
-							</div>
-						</Link>
-					))}
-			</div>
-		</section>
-	);
+  return (
+    <section className="px-8 py-15 lg:px-10">
+      <h1 className="text-slate-800 mb-15 lg:mb-10 font-semibold text-3xl lg:text-4xl text-center">
+        Shop by Category
+      </h1>
+      <div className="px-5 sm:px-0 grid grid-cols-1 gap-14 sm:grid-cols-4 md:gap-10 lg:max-w-3/4 lg:mx-auto">
+        {categories &&
+          categories.map((cat, index) => (
+            <Link to={`category/${cat.name.toLowerCase()}`} key={index}>
+              <div
+                className="flex flex-col justify-between items-center gap-5 w-full h-full px-5 py-0 lg:py-12"
+                key={index}
+              >
+                <div className="aspect-square w-full relative bg-white">
+                  <img
+                    className="absolute inset-0 w-full h-full object-contain"
+                    src={cat.image}
+                    alt={cat.name}
+                  />
+                </div>
+
+                <span className="block text-slate-700 text-sm md:text-base font-medium">
+                  {cat.name}
+                </span>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </section>
+  );
 }
 
 export default Categories;
+
+/*
+return (
+    <section className="px-8 py-15 lg:px-10">
+      <h1 className="text-slate-800 mb-15 lg:mb-10 font-semibold text-3xl lg:text-4xl text-center">
+        Shop by Category
+      </h1>
+      <div className="px-15 sm:px-0 grid grid-cols-1 gap-14 sm:grid-cols-4 md:gap-10 lg:max-w-3/4 lg:mx-auto">
+        {categories &&
+          categories.map((cat, index) => (
+            <Link to={`category/${cat.name.toLowerCase()}`} key={index}>
+              <div
+                className="flex flex-col justify-between items-center gap-5 px-5 py-0 lg:py-12"
+                key={index}
+              >
+                <div className="aspect-square bg-white">
+                  <img
+                    className="w-full h-full object-contain"
+                    src={cat.image}
+                  />
+                </div>
+
+                <span className="text-slate-700 text-sm md:text-base font-medium">
+                  {cat.name}
+                </span>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </section>
+  );
+*/
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,37 +1,68 @@
 import React from "react";
 
 function Login() {
-	return (
-		<div className="mt-20 w-full flex justify-center items-center">
-			<div className="w-80 md:w-96 flex flex-col justify-items-start">
-				<h1 className="mb-10 text-center text-slate-800 text-2xl font-bold tracking-tight">Sign in to your account</h1>
-				<div>
-					<form action="#">
-						<div>
-							<label htmlFor="email" className="text-sm font-medium">
-								Email address
-							</label>
-							<div className="mt-2">
-								<input type="email" name="email" id="email" placeholder="example@example.com" autoComplete="email" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-slate-600 sm:text-sm/6" />
-							</div>
-						</div>
-						<div className="mt-5">
-							<label htmlFor="password" className="text-sm font-medium">
-								Password
-							</label>
-							<div className="mt-2">
-								<input type="password" name="password" id="password" placeholder="password" autoComplete="password" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-slate-600 sm:text-sm/6" />
-							</div>
-						</div>
-						<button className="w-full mt-5 bg-slate-500 hover:bg-slate-600 transition duration-300 text-slate-50 py-2 rounded-md cursor-pointer">Sign In</button>
-						<p className="mt-5 text-slate-500 text-center tracking-tight">
-							Don't have an account? <button className="text-slate-600 hover:text-indigo-800 transition duration-300 font-medium cursor-pointer">Sign up</button>
-						</p>
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+  const submitLogin = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+
+  return (
+    <div className="mt-20 w-full flex justify-center items-center">
+      <div className="w-80 md:w-96 flex flex-col justify-items-start">
+        <h1 className="mb-10 text-center text-slate-800 text-2xl font-bold tracking-tight">
+          Sign in to your account
+        </h1>
+        <div>
+          <form onSubmit={submitLogin}>
+            <div>
+              <label htmlFor="email" className="text-sm font-medium">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="example@example.com"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-slate-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div className="mt-5">
+              <label htmlFor="password" className="text-sm font-medium">
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password"
+                  autoComplete="password"
+                  required
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-slate-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full mt-5 bg-slate-500 hover:bg-slate-600 transition duration-300 text-slate-50 py-2 rounded-md cursor-pointer"
+            >
+              Sign In
+            </button>
+            <p className="mt-5 text-slate-500 text-center tracking-tight">
+              Don't have an account?{" "}
+              <button className="text-slate-600 hover:text-indigo-800 transition duration-300 font-medium cursor-pointer">
+                Sign up
+              </button>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
